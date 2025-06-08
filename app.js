765327c7f019398e9718e1f707506ffdcb632471
Robssuttie123 <robssuttie123@gmail.com>
2025-06-08 17:30:24 +0100

    Update app.js - overlay fix 1.5

--- app.js
+++ app.js
@@ -33,14 +33,6 @@ function authMiddleware(req, res, next) {
   }
 }
 
-app.get('/api/overlay-id', (req, res) => {
-  if (req.session && req.session.overlayId) {
-    res.json({ overlayId: req.session.overlayId });
-  } else {
-    res.status(404).json({ error: 'Overlay ID not found' });
-  }
-});
-
 app.get('/overlay', (req, res) => {
   res.sendFile(path.join(__dirname, 'overlay.html'));
 });
